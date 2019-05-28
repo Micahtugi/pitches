acd4c63c6cc5ae2714daa11f9a659ab3114e4efc
Micah <micahkimathi@gmail.com>
2019-05-28 10:14:27 +0300

    Initial folder structure

---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+
+class Config:
+    """Main configurations class"""
+
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+
+    SECRET_KEY = "tugi"
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+
+
+
+class ProdConfig(Config):
+    """Production configuration class that inherits from the main configurations class"""
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")#for heroku purposes
+
+class DevConfig(Config):
+    """Configuration class for development stage of the app"""
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://micah:kioko@localhost/db001'
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,   
+    'production': ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='tugi'
+export MAIL_USERNAME='micahkimathi@gmail.com'
+export MAIL_PASSWORD='tugi2018'
+
+python3.6 manage.py server  
