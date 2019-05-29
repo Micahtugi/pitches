83ae51411ed5aac24e0fe077ff2d2bb3b7393fe4
Micah <micahkimathi@gmail.com>
2019-05-29 11:01:57 +0300

    Created an email.py module in the app directory

--- app/__init__.py
+++ app/__init__.py
@@ -9,11 +9,14 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 
+mail = Mail()
 
 
 def create_app(config_name):
 
     app = Flask(__name__)
+    mail.init_app(app)
+    
 
     # Creating the app configurations.
     app.config.from_object(config_options[config_name])
